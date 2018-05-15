6cba8743ab065c44614d2e4f798f9b88a3f663ed
David Kivlehan <funzeye@gmail.com>
2018-05-15 13:23:00 +0100

    Add project files.

---
+++ WeatherMicroservice/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherMicroservice
+{
+    public static class Extensions
+    {
+        public static double? TryParse(this string input)
+        {
+            double result;
+            if (double.TryParse(input, out result))
+            {
+                return result;
+            }
+            else
+            {
+                return default(double?);
+            }
+        }
+    }
+}
